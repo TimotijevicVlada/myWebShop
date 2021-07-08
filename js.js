072014eb61c2b8c714074b5513fcdf98be414b51
Vladimir <timotijevicvlada@gmail.com>
2021-07-08 15:52:02 +0200

    Resen problem sa lisnerima. Preuredjen div show Details

--- js.js
+++ js.js
@@ -110,7 +110,7 @@ function fetchJson(json, container) {
             <div class="shopping-card shop-item">
                 <h3 class="shop-item-title">${object[i].name}</h3>
                 <img class="shop-item-image ${object[i].imageSize}" src="${object[i].image}" alt="">
-                <p>${printStars(object[i].stars)}</p>
+                <p class="shop-item-stars">${printStars(object[i].stars)}</p>
                 <p class="shop-item-naStanju">${object[i].naStanju ? "Proizvod je dostupan" : "Proizvod nije dostupan"}</p>
                 <p>Materijal: <span class="boldovano shop-item-material">${object[i].material}</span></p>
                 <p>Garancija: <span class="boldovano shop-item-guaranty">${object[i].guaranty}</span></p>
@@ -125,6 +125,18 @@ function fetchJson(json, container) {
     }
     container.innerHTML = html;
     
+    //Liseneri za add to cart button
+    let addButton = document.getElementsByClassName("shop-item-button");
+    for (let item of addButton) {
+        item.addEventListener("click", addItem);
+    }
+
+    //Liseneri za show details button
+    let detailsBtn = document.getElementsByClassName("details-btn");
+    for(let i = 0; i < detailsBtn.length; i++) {
+        detailsBtn[i].addEventListener("click", showDetails);
+    }
+
     }).catch(function(error) {
         console.log(error);
     })
@@ -311,3 +323,161 @@ liseneri(searchInput, "keyup");
 liseneri(sortAvailable, "change");
 liseneri(filterDostupno, "change");
 
+/*================================================================================================*/
+//Funkcije za Details prozor
+const bluredWindow = document.getElementsByClassName("blurred-div")[0];
+const container = document.getElementsByClassName("details-inner")[0];
+
+function exitDetails() {
+    bluredWindow.style.marginLeft = "-100%";
+    container.innerHTML = "";
+}
+
+function showDetails(items) {
+  let btn = items.target;
+  let productDiv = btn.parentElement.parentElement;
+  let title = productDiv.getElementsByClassName("shop-item-title")[0].innerText;
+  let price = productDiv.getElementsByClassName("shop-item-price")[0].innerText;
+  let picture = productDiv.getElementsByClassName("shop-item-image")[0].src;
+  let state = productDiv.getElementsByClassName("shop-item-naStanju")[0].innerText;
+  let material = productDiv.getElementsByClassName("shop-item-material")[0].innerText;
+  let guaranty = productDiv.getElementsByClassName("shop-item-guaranty")[0].innerText;
+  let available = productDiv.getElementsByClassName("shop-item-dostupno")[0].innerText;
+  let stars = productDiv.getElementsByClassName("shop-item-stars")[0].innerHTML;
+
+  let html = "";
+
+  html = `
+      <i class="fas fa-times cancel-blurred"></i>
+      <div class="details-img">
+        <img src="${picture}">
+      </div>
+      <div class="details-info">
+        <h1 class="details-title">${title}</h1>
+        <div class="details-middle-div">
+          <p class="details-state">${state}</p>
+          <p class="details-material"><span>Material:</span> ${material}</p>
+          <p class="details-guaranty"><span>Guaranty:</span> ${guaranty}</p>
+          <p class="details-available"><span>Available:</span> ${available}</p>
+        </div>
+        <p class="details-stars">${stars}</p>
+        <p class="details-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat deserunt facere quisquam mollitia eaque blanditiis ducimus esse sequi ex. Libero dicta doloribus fuga nemo corrupti fugit eius officiis quasi ipsam.</p>
+        <div class="details-price">
+          <p>Price: <span>${price}</span></p>
+        </div>
+      </div>
+  `;
+  container.innerHTML = html;
+  bluredWindow.style.marginLeft = "0%";
+
+  let exitBlured = document.getElementsByClassName("cancel-blurred")[0];
+    exitBlured.addEventListener("click" , exitDetails);
+}
+
+/*===========================================================================================*/
+//Funkcije za Add to Cart btn
+document.getElementsByClassName("order-btn")[0].addEventListener("click", orderProducts); 
+
+function orderProducts() {
+  let kupi = document.getElementsByClassName("cart-items")[0];
+  if (kupi.hasChildNodes()) {
+    //Provera da li u korpi ima artikala
+    document
+      .getElementsByClassName("order-btn")[0]
+      .addEventListener("click", (window.location.href = "form.html"));
+  } else {
+    alert("Niste odabrali artikle!"); //Ako nema artikala ispisi alert "Niste odabrali artikle!"
+  }
+  let cartItems = document.getElementsByClassName("cart-items")[0]; //Div koji je prazan i u koji treba da se ubace proizvodi
+  while (cartItems.hasChildNodes()) {
+    //Ako ovaj div ima childove...
+    cartItems.removeChild(cartItems.firstChild);
+  }
+  updatePrice();
+}
+
+function addItem(event) {
+  //Funkcija za dodavanje artikala u korpu
+  let dugme = event.target; //Target isto kao opcija "this" samo sto sa target gadjamo bas taj element a sa this roditeljski
+  let shopItem = dugme.parentElement.parentElement;
+  let naslov = shopItem.getElementsByClassName("shop-item-title")[0].innerText;
+  let cena = shopItem.getElementsByClassName("shop-item-price")[0].innerText;
+  let slika = shopItem.getElementsByClassName("shop-item-image")[0].src;
+  let naStanju = shopItem.getElementsByClassName("shop-item-naStanju")[0].innerText;
+  let materijal = shopItem.getElementsByClassName("shop-item-material")[0].innerText;
+  let guaranty = shopItem.getElementsByClassName("shop-item-guaranty")[0].innerText;
+  let dostupno = shopItem.getElementsByClassName("shop-item-dostupno")[0].innerText;
+  console.log(shopItem);
+  addToCart(naslov, cena, slika, naStanju, materijal, guaranty, dostupno); //Ove parametre treba da prosledimo u korpu
+  updatePrice();
+}
+
+function addToCart(naslov, cena, slika, naStanju, materijal, guaranty, dostupno) {
+  let cartRow = document.createElement("div"); //cartRow Je red koji treba da se pojavi kada ubacimo u korpu (Za sada se on ne vidi)
+  cartRow.classList.add("cart-row"); //Moramo da mu dodamo klasu kako bi ga formatirali
+  let cartItems = document.querySelector(".cart-items"); //Dohvatamo cartItems
+  let naslovProvera = cartItems.getElementsByClassName("cart-item-title"); //Provera sa IF da ne bi ubacivali dva puta isti element u korpu
+  for (let i = 0; i < naslovProvera.length; i++) {
+    if (naslovProvera[i].innerText == naslov) {
+      //Ako je naslov jednako naslov onda ne ubacuj u korpu
+      alert("Ovaj proizvod ste vec ubacili u korpu!");
+      return; //Returnom izlazimo iz ove funkcije
+    }
+  }
+  //Pravimo elemente koji ce da se ispisuju nakon klika na Dodaj u korpu
+  let ispis = `<div class='cart-item cart-column' >     
+                  <img class='cart-item-image' src="${slika}">
+                  <span class="cart-item-title">${naslov}</span>
+                 </div>
+                  <span class="cart-price cart-column">${cena}</span>
+                 <div class="cart-item-description">
+                  <p><i class="fas fa-angle-right"></i> ${naStanju ? "Proizvod je dostupan" : "Proizvod nije dostupan"}</p>
+                  <p><i class="fas fa-angle-right"></i> Materijal: <span class="boldovano">${materijal}</span></p>
+                  <p><i class="fas fa-angle-right"></i> Garancija: <span class="boldovano">${guaranty}</span></p>
+                  <p><i class="fas fa-angle-right"></i> Dostupno: <span class="boldovano">${dostupno}</span></p>
+                 </div> 
+                 <div class="cart-quantity  cart-column">
+                    <input type="number" class="cart-quantity-input" value="1" max="10">
+                    <button class="btn btn-remove" type="button"><i class="fas fa-trash"></i></button>
+                 </div>                
+              `;
+  cartRow.innerHTML = ispis;
+  cartItems.append(cartRow);
+  cartRow
+    .getElementsByClassName("btn-remove")[0]
+    .addEventListener("click", deleteItem);
+  cartRow
+    .getElementsByClassName("cart-quantity-input")[0]
+    .addEventListener("change", changeQuantity);
+}
+
+function deleteItem(event) {
+  let dugme = event.target;
+  dugme.parentElement.parentElement.parentElement.remove(); //Brisemo ceo red tako da moramo da se vratimo 3 koraka u nazad
+  updatePrice();
+}
+
+function updatePrice() {
+  let totalCount = document.getElementsByClassName("brojac-proizvoda")[0]; //Brojac proizvoda na korpi
+  let cartItems = document.querySelector(".cart-items");
+  let cartRows = cartItems.getElementsByClassName("cart-row");
+  let suma = 0; //Treba nam brojac kako bi sabirali cenu
+  for (let i = 0; i < cartRows.length; i++) {
+    let cenaE = cartRows[i].querySelector(".cart-price").innerText;
+    let kolicina = cartRows[i].querySelector(".cart-quantity-input").value;
+    let cena = parseFloat(cenaE.replace(".", ""));  //Sklanjam tacku da bi sabrao realno sve cene
+    suma = suma + cena * kolicina; //Funkcija za sabiranje cene i kolicine proizvoda
+  }
+  totalCount.innerText = cartRows.length; //Broj proizvoda je broj cartRows elemenata
+  document.getElementsByClassName("cart-total-price")[0].innerText =
+    suma.toFixed(2); //dobijena suma
+}
+
+function changeQuantity(event) {
+  let input = event.target; //Razlika izmedju target i this je ta sto sa this gadjamo roditeljski element
+  if (isNaN(input.value) || input.value <= 0) {
+    //a sa targetom bas taj element koji je u dogadjaju!
+    input.value = 1; //sve ovo moze da se resi i sa this opcijom
+  }
+  updatePrice();
+}
